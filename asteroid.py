a39754b55d3137250075e02b337783e8813b2e01
whaytogo <whaytogo@gmail.com>
2024-09-01 21:20:15 +0400

    commit 5

--- asteroid.py
+++ asteroid.py
@@ -14,4 +14,28 @@ class Asteroid (CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
 
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            x, y = self.position
+            spawn_angle = random.uniform(20,50)
+            
+            # setting velocity for children
+            velocity1 = self.velocity.rotate(spawn_angle)
+            velocity2 = self.velocity.rotate(-spawn_angle)
+
+            # calculating radius
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            
+            # spawning the children
+            first_child = Asteroid(x, y,new_radius)
+            second_child = Asteroid(x,y,new_radius)
+            
+            # velocity for spawns
+            first_child.velocity = velocity1 * 1.2
+            second_child.velocity = velocity2 * 1.2
+
+
 
--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def main():
 				sys.exit()
 			for shot in shots:
 				if shot.collide(asteroid):
-					asteroid.kill()
+					asteroid.split()
 					shot.kill()
 		
 
